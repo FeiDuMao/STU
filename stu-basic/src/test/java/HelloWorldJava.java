cc10cd82870425fcce75b629c83e61e0b7a1410a
FeiDuMao <1936184791@qq.com>
2023-02-18 00:16:38 +0800

    windows commit

--- stu-basic/src/test/java/HelloWorldJava.java
+++ stu-basic/src/test/java/HelloWorldJava.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-public class HelloWorld {
+public class HelloWorldJava {
     @Test
     public void test(){
         System.out.println("hello world!");
--- stu-basic/src/test/kotlin/HelloWorldKotlin.kt
+++ stu-basic/src/test/kotlin/HelloWorldKotlin.kt
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Test
 
-class HelloWorld {
+class HelloWorldKotlin {
 
     @Test
     fun test(){
